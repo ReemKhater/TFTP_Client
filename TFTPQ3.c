f62d16567187a1171c376b26fd7de13364e118ed
ensea <ensea@StudentLab>
2024-12-16 12:00:37 +0100

    Update the code

--- TFTPQ3.c
+++ TFTPQ3.c
@@ -16,17 +16,17 @@ void PrintPrompt(const char *prompt,int size){
 	write(STDOUT_FILENO,prompt,size); //Write in the shell the right prompt
 	}
 
-void ConnectToServer(const char *domain, const char *port){
+int ConnectToServer(const char *domain, const char *port, int *sockfd){
 	struct addrinfo hints;
 	struct addrinfo *res;
     struct addrinfo *rp;
-    int sockfd;
 	
 	size_t sizeHint=sizeof(hints);
 	
 	memset(&hints, ZERO, sizeHint);
 	
 	hints.ai_family = AF_UNSPEC; //Allowing IPv4 and IPv6
+	hints.ai_socktype = SOCK_DGRAM; // Only UDP addresses
 	
 	
 	int ecode= getaddrinfo(domain,port,&hints,&res);
@@ -43,11 +43,11 @@ void ConnectToServer(const char *domain, const char *port){
 		struct sockaddr_in *new_addr=(struct sockaddr_in *)rp->ai_addr;
 		addr=&(new_addr->sin_addr);
 		
-		sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol); //Create a socket
+		*sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol); //Create a socket
 			
 		inet_ntop(rp->ai_family, addr, addr_str, rp->ai_addrlen); //Convert the struct into a string
 		
-		if (connect(sockfd, rp->ai_addr, rp->ai_addrlen) == 0) { //Trying to connect to the server
+		if (connect(*sockfd, rp->ai_addr, rp->ai_addrlen) == 0) { //Trying to connect to the server
             const char *answer="Connected to the server. \n";
 			int sizeAnswer=strlen(answer);
 		
@@ -56,15 +56,18 @@ void ConnectToServer(const char *domain, const char *port){
 	 }
 	 
 	 freeaddrinfo(res);
+	 return *sockfd;
 }
 
 void gettftp(const char *domain, char file,const char *port){
+	int sockfd=-1;
 	const char *prompt="Connection to the server...\n";
 	int sizePrompt=strlen(prompt);
 	
 	PrintPrompt(prompt,sizePrompt);
 	
-	ConnectToServer(domain,port);
+	ConnectToServer(domain,port,&sockfd);
+	printf("%d \n",sockfd);
 	
 	const char *downloading="download of the file...\n";
 	int sizeDownloading= strlen(downloading);
@@ -79,29 +82,42 @@ void gettftp(const char *domain, char file,const char *port){
 	
 	PrintPrompt(done,sizeDone);	
 	
+	close(sockfd); //Closing of the socket
+	const char *closing="Socket closed \n";
+	int sizeClosing= strlen(closing);
+	
+	PrintPrompt(closing,sizeClosing);	
+	
 	
 }
 
 void puttftp(const char *domain, char file, const char *port){
+	int sockfd=-1;
 	const char *prompt="Connection to the server...\n";
 	int sizePrompt=strlen(prompt);
 	
 	PrintPrompt(prompt,sizePrompt);
 	
-	ConnectToServer(domain,port);
+	ConnectToServer(domain,port,&sockfd);
 	
-	const char *downloading="upload of the file...\n";
-	int sizeDownloading= strlen(downloading);
+	const char *uploading="upload of the file...\n";
+	int sizeUploading= strlen(uploading);
 	
-	PrintPrompt(downloading,sizeDownloading);
+	PrintPrompt(uploading,sizeUploading);
 	
-	//Here the code to download the file
+	//Here the code to upload the file
 	printf("%d \n",file);
 
 	const char *done="file upload. \n";
 	int sizeDone= strlen(done);
 	
 	PrintPrompt(done,sizeDone);	
+	
+	close(sockfd); //Closing of the socket
+	const char *closing="Socket closed \n";
+	int sizeClosing= strlen(closing);
+	
+	PrintPrompt(closing,sizeClosing);	
 	
 	
 }
@@ -116,6 +132,7 @@ int main(int argc, char ** argv){
 	}
 	const char *port = "69";
 	const char *domain = "srvtpinfo1.ensea.fr";
+	
 	if(strcmp(argv[0],GET)==0){ //If using gettftp
 		gettftp(domain, *argv[2],port);
 	}else if(strcmp(argv[0],PUT)==0){ //If using puttftp
--- TFTPQ4.c
+++ TFTPQ4.c
@@ -11,13 +11,21 @@
 #define ZERO 0
 #define GET "./gettftp"
 #define PUT "./puttftp"
-
+#define SIZE_OF_PACKET 512
 
 void PrintPrompt(const char *prompt,int size){
 	write(STDOUT_FILENO,prompt,size); //Write in the shell the right prompt
 	}
+	
+int ConvertToASCII(const char *file, unsigned char *ASCIIfile){
+    for(unsigned int i = 0; i < strlen(file); ++i)
+    {
+        ASCIIfile[i]=(unsigned char) file[i];
+    }
+    return *ASCIIfile;
+}
 
-char getaddr(const char *domain, char *addr_str){
+int ConnectToServer(const char *domain, const char *port, int *sockfd){
 	struct addrinfo hints;
 	struct addrinfo *res;
     struct addrinfo *rp;
@@ -30,7 +38,7 @@ char getaddr(const char *domain, char *addr_str){
 	hints.ai_socktype = SOCK_DGRAM; // Only UDP addresses
 	
 	
-	int ecode= getaddrinfo(domain,NULL,&hints,&res);
+	int ecode= getaddrinfo(domain,port,&hints,&res);
 
 	if(ecode!=0){
 		printf("Error: %s \n",gai_strerror(ecode));
@@ -38,26 +46,52 @@ char getaddr(const char *domain, char *addr_str){
 	}
 	
 	void *addr;
+	char addr_str[INET6_ADDRSTRLEN];
 	
 	for (rp = res; rp != NULL; rp = rp->ai_next) {
 		struct sockaddr_in *new_addr=(struct sockaddr_in *)rp->ai_addr;
 		addr=&(new_addr->sin_addr);
+		
+		*sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol); //Create a socket
 			
 		inet_ntop(rp->ai_family, addr, addr_str, rp->ai_addrlen); //Convert the struct into a string
-		//printf("IP address is %s \n",addr_str);
+		
+		if (connect(*sockfd, rp->ai_addr, rp->ai_addrlen) == 0) { //Trying to connect to the server
+            const char *answer="Connected to the server. \n";
+			int sizeAnswer=strlen(answer);
+		
+			PrintPrompt(answer,sizeAnswer);
+		}
 	 }
+	 
 	 freeaddrinfo(res);
-	 return *addr_str;
+	 return *sockfd;
 }
 
-void gettftp(const char *domain, char file){
+void gettftp(const char *domain, const char *file,const char *port){
+	int sockfd=-1;
 	const char *prompt="Connection to the server...\n";
 	int sizePrompt=strlen(prompt);
+	int rrq_packet[SIZE_OF_PACKET];
+	unsigned char ASCIIfile[strlen(file)];
+	
+	//Initialization of the first two bits of rrq
+	rrq_packet[0]=ZERO;
+	rrq_packet[1]=1;
+	printf("%d \n",rrq_packet[0]);
 	
 	PrintPrompt(prompt,sizePrompt);
 	
-	//Here the code to connect to the server
-	printf("%s \n",domain);
+	ConvertToASCII(file,ASCIIfile); //Convert file in ASCII 
+	
+	for(unsigned int i=0; i<strlen(file);i++){ //Add the n bytes of the filename
+		rrq_packet[2+i]=ASCIIfile[i];
+	}
+	
+	//Next, continue to configure the RRQ
+	
+	ConnectToServer(domain,port,&sockfd);
+	printf("the socket is %d \n",sockfd);
 	
 	const char *downloading="download of the file...\n";
 	int sizeDownloading= strlen(downloading);
@@ -65,42 +99,54 @@ void gettftp(const char *domain, char file){
 	PrintPrompt(downloading,sizeDownloading);
 	
 	//Here the code to download the file
-	printf("%d \n",file);
+	printf("%s \n",file);
 
 	const char *done="file download. \n";
 	int sizeDone= strlen(done);
 	
 	PrintPrompt(done,sizeDone);	
 	
+	close(sockfd); //Closing of the socket
+	const char *closing="Socket closed \n";
+	int sizeClosing= strlen(closing);
+	
+	PrintPrompt(closing,sizeClosing);	
+	
 	
 }
 
-void puttftp(const char *domain, char file){
+void puttftp(const char *domain, const char *file, const char *port){
+	int sockfd=-1;
 	const char *prompt="Connection to the server...\n";
 	int sizePrompt=strlen(prompt);
 	
 	PrintPrompt(prompt,sizePrompt);
 	
-	//Here the code to connect to the server
-	printf("%s \n",domain);
+	ConnectToServer(domain,port,&sockfd);
 	
 	const char *uploading="upload of the file...\n";
-	int sizeUploadng= strlen(uploading);
+	int sizeUploading= strlen(uploading);
 	
 	PrintPrompt(uploading,sizeUploading);
 	
 	//Here the code to upload the file
-	printf("%d \n",file);
+	printf("%s \n",file);
 
 	const char *done="file upload. \n";
 	int sizeDone= strlen(done);
 	
 	PrintPrompt(done,sizeDone);	
 	
+	close(sockfd); //Closing of the socket
+	const char *closing="Socket closed \n";
+	int sizeClosing= strlen(closing);
+	
+	PrintPrompt(closing,sizeClosing);	
+	
 	
 }
 
-int main(int argc, char ** argv){
+int main(int argc, const char ** argv){
 	if(argc<3){
 		const char *prompt="not enough argument \n";
 		int sizePrompt=strlen(prompt);
@@ -108,17 +154,12 @@ int main(int argc, char ** argv){
 		PrintPrompt(prompt,sizePrompt);
 		exit(EXIT_FAILURE);
 	}
-	const char *domain = "srvtpinfo1.ensea.fr";
-	char addr_str[INET6_ADDRSTRLEN];
+	const char *port = "69";
 	
-	getaddr(domain, addr_str); //recovering of the IP address of the DNS in addr_str
-    printf("The IP address is %s \n", addr_str);
-    
-
 	if(strcmp(argv[0],GET)==0){ //If using gettftp
-		gettftp(domain, *argv[2]);
+		gettftp(argv[1], argv[2],port);
 	}else if(strcmp(argv[0],PUT)==0){ //If using puttftp
-		puttftp(domain, *argv[2]);
+		puttftp(argv[1], argv[2],port);
     }else{
 		const char *prompt="Function doesn't exist \n";
 		int sizePrompt=strlen(prompt);
